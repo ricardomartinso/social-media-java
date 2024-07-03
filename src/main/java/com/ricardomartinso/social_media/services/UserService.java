4831d02a444f1ee0aebdbfa8dd2a0478590f8857
Ricardo Martins <ricardomartinso.dev@gmail.com>
2024-07-03 18:18:46 -0300

    feat: creates UserService and PostService

---
+++ src/main/java/com/ricardomartinso/social_media/services/UserService.java
@@ -0,0 +1,28 @@
+package com.ricardomartinso.social_media.services;
+
+import com.ricardomartinso.social_media.model.user.User;
+import com.ricardomartinso.social_media.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserByUsername(String username) {
+        Optional<User> user = userRepository.findByUsername(username);
+
+        if (user.isEmpty()) {
+            throw new RuntimeException("Usuário não encontrado");
+        }
+
+        return user.get();
+
+    }
+}
