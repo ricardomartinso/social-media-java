7c39535cf33bab6d0854eb27a112c338c0a13897
Ricardo Martins <ricardomartinso.dev@gmail.com>
2024-07-03 18:11:57 -0300

    feat: adds table post comments to migration

---
+++ src/main/resources/db/migration/V2__creating_table_tab_post_comments.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tab_post_comments (
+                                   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                   post_id BIGINT NOT NULL,
+                                   comment TEXT,
+                                   user_id BIGINT,
+                                   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                   CONSTRAINT fk_post FOREIGN KEY (post_id) REFERENCES tab_posts(id),
+                                   CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES tab_users(id)
+);
