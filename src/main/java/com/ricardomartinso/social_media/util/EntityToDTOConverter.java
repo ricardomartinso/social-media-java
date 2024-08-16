f91222840766c2f27d42dc2bdb9923add1ae8b32
Ricardo Martins <ricardomartinso.dev@gmail.com>
2024-08-16 15:47:58 -0300

    feat: adds converter (PostComment to PostCommentDTO)

--- src/main/java/com/ricardomartinso/social_media/util/EntityToDTOConverter.java
+++ src/main/java/com/ricardomartinso/social_media/util/EntityToDTOConverter.java
@@ -1,8 +1,10 @@
 package com.ricardomartinso.social_media.util;
 
+import com.ricardomartinso.social_media.dtos.PostCommentDTO;
 import com.ricardomartinso.social_media.dtos.PostDTO;
 import com.ricardomartinso.social_media.dtos.UserDTO;
 import com.ricardomartinso.social_media.model.post.Post;
+import com.ricardomartinso.social_media.model.post.PostComment;
 import com.ricardomartinso.social_media.model.user.User;
 
 public class EntityToDTOConverter {
@@ -13,5 +15,9 @@ public class EntityToDTOConverter {
 
     public static UserDTO convertToUserDTO(User user) {
         return new UserDTO(user.getUsername(), user.getEmail());
+    }
+
+    public static PostCommentDTO convertToPostCommentDTO(PostComment postComment) {
+        return new PostCommentDTO(postComment.getUser().getId(), postComment.getUser().getImageUrl() ,postComment.getComment(), postComment.getCreatedAt());
     }
 }
