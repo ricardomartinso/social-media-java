ad7a127f5f41c09f8f1dfb3d66d5fee7434fc22d
Ricardo Martins <ricardomartinso.dev@gmail.com>
2024-10-09 11:00:03 -0300

    chore: add RsaKeyProperties configurations and SecurityConfig

--- src/main/java/com/ricardomartinso/social_media/SocialMediaApplication.java
+++ src/main/java/com/ricardomartinso/social_media/SocialMediaApplication.java
@@ -1,8 +1,11 @@
 package com.ricardomartinso.social_media;
 
+import com.ricardomartinso.social_media.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class SocialMediaApplication {
 
