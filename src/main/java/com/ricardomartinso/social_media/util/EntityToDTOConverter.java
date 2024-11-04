75cb8a70d2e471b4338c5358f6339fcb820f9172
Ricardo Martins <ricardomartinso.dev@gmail.com>
2024-11-04 17:36:42 -0300

    refactor: adjusts Entity converter.

--- src/main/java/com/ricardomartinso/social_media/util/EntityToDTOConverter.java
+++ src/main/java/com/ricardomartinso/social_media/util/EntityToDTOConverter.java
@@ -1,23 +1,23 @@
 package com.ricardomartinso.social_media.util;
 
-import com.ricardomartinso.social_media.dtos.PostCommentDTO;
-import com.ricardomartinso.social_media.dtos.PostDTO;
-import com.ricardomartinso.social_media.dtos.UserDTO;
-import com.ricardomartinso.social_media.model.post.Post;
-import com.ricardomartinso.social_media.model.post.PostComment;
-import com.ricardomartinso.social_media.model.user.User;
+import com.ricardomartinso.social_media.post.PostCommentDetail;
+import com.ricardomartinso.social_media.post.PostDTO;
+import com.ricardomartinso.social_media.user.UserDTO;
+import com.ricardomartinso.social_media.post.Post;
+import com.ricardomartinso.social_media.post.PostComment;
+import com.ricardomartinso.social_media.user.User;
 
 public class EntityToDTOConverter {
 
     public static PostDTO convertToPostDTO(Post post) {
-        return new PostDTO(post.getId(), post.getContent(), post.getImageUrl(), post.getCreatedAt());
+        return new PostDTO(post.getId(), post.getUser().getUsername(), post.getContent(), post.getImageUrl(), post.getCreatedAt());
     }
 
     public static UserDTO convertToUserDTO(User user) {
         return new UserDTO(user.getUsername(), user.getEmail());
     }
 
-    public static PostCommentDTO convertToPostCommentDTO(PostComment postComment) {
-        return new PostCommentDTO(postComment.getUser().getId(), postComment.getUser().getImageUrl() ,postComment.getComment(), postComment.getCreatedAt());
+    public static PostCommentDetail convertToPostCommentDTO(PostComment postComment) {
+        return new PostCommentDetail(postComment.getUser().getId(), postComment.getUser().getImageUrl() ,postComment.getComment(), postComment.getCreatedAt());
     }
 }
