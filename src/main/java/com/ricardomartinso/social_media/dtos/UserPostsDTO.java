9cc537c2c9d07eda1b2b44cbc84eb6eb22493413
Ricardo Martins <ricardomartinso.dev@gmail.com>
2024-11-04 17:28:59 -0300

    removes unused classes

--- src/main/java/com/ricardomartinso/social_media/dtos/UserPostsDTO.java
+++
@@ -1,6 +0,0 @@
-package com.ricardomartinso.social_media.dtos;
-
-import java.util.List;
-
-public record UserPostsDTO (UserDTO user, List<PostDTO> posts) {
-}
--- src/main/java/com/ricardomartinso/social_media/dtos/response/PostCommentsResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package com.ricardomartinso.social_media.dtos.response;
-
-import com.ricardomartinso.social_media.dtos.PostCommentDTO;
-import org.springframework.http.HttpStatus;
-
-import java.util.List;
-
-public record PostCommentsResponseDTO(String message, List<PostCommentDTO> data, HttpStatus statusCode) {
-}
